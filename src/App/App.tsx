731c07c814ecef22c0dc0059c34f55ae7e5d1856
Paolo Cattani <paolocattani0501@gmail.com>
2019-04-27 16:21:41 +0200

    Modified folder structure
    Modified folder structure

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './style/logo.svg';
-import './style/App.css';
+import logo from '../style/logo.svg';
+import '../style/App.css';
 
 const App: React.FC = () => {
   return (
